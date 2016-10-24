3a419b00af06745abcfcdb87400d92fb2f21d816
Phung <phungnet001@gmail.com>
2016-10-24 14:31:38 +0700

    Circle Demo

---
+++ Phung_demo/Phung_demo/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Phung_demo
+//
+//  Created by Phung on 10/24/16.
+//  Copyright © 2016 Phung. All rights reserved.
+//
+
+import UIKit
+import Pulsator
+
+class ViewController: UIViewController {
+    
+    let pulsator = Pulsator()
+    
+    @IBOutlet weak var imagePhone: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //config Circle
+        configPulsator()
+        
+        //add circle to image
+        imagePhone.layer.superlayer?.insertSublayer(pulsator, below: imagePhone.layer)
+        
+        //Start running cirle
+        pulsator.start()
+        
+    }
+    
+    func configPulsator(){
+        
+        //Number of circle
+        pulsator.numPulse = 10
+        
+        //Size of circle
+        pulsator.radius = 150.0
+        
+        //Duration of animation of each cirle
+        pulsator.animationDuration = 5
+        
+        //Color of circle
+        pulsator.backgroundColor = UIColor(
+            red: CGFloat(0.0),
+            green: CGFloat(0.4),
+            blue: CGFloat(0.7),
+            alpha: CGFloat(1.0)).cgColor
+        
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //config layer
+        imagePhone.layer.layoutIfNeeded()
+        //config position of Circle
+        pulsator.position = imagePhone.layer.position
+    }
+    
+    
+    
+}
+
---
+++ Phung_demo/Pods/Target Support Files/Pods-Phung_demo/Pods-Phung_demo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Phung_demo : NSObject
+@end
+@implementation PodsDummy_Pods_Phung_demo
+@end
