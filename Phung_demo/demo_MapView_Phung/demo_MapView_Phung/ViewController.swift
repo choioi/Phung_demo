6da403976a028f02c817f2d6ac7c0c6213cde79a
Phung <phungnet001@gmail.com>
2016-10-24 14:49:48 +0700

    Circle Demo 2

---
+++ Phung_demo/demo_MapView_Phung/demo_MapView_Phung/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  demo_MapView_Phung
+//
+//  Created by Phung on 10/24/16.
+//  Copyright © 2016 Phung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+               // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
