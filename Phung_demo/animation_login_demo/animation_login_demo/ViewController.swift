d036e05a1a9b53ef28105cdae5bb741d31828359
Phung <phungnet001@gmail.com>
2016-10-26 17:38:52 +0700

    Gradient background

--- Phung_demo/animation_login_demo/animation_login_demo/ViewController.swift
+++ Phung_demo/animation_login_demo/animation_login_demo/ViewController.swift
@@ -26,16 +26,31 @@ class ViewController: UIViewController {
             
             underKeyboardLayoutConstraint.setup(bot, view: view,
                                                 bottomLayoutGuide: bottomLayoutGuide)
-            changeGradient()
+            setGradientBackground()
+            //or changeGradient()
 
            
         }
     
+    //1
     func changeGradient() {
         view.backgroundColor = UIColor.clear
         let backgroundLayer = colors.gl
         backgroundLayer.frame = view.frame
         view.layer.insertSublayer(backgroundLayer, at: 0)
+    }
+    //2
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [ colorTop, colorBottom]
+        gradientLayer.locations = [ 0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+        
+        //self.view.layer.addSublayer(gradientLayer)
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
 
